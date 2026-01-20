7f002feaecb1a3a79a0293ca7f054013d6428a16
Sandeep Sharma <skumarsharma@insulet.com>
2026-01-20 13:25:36 +0530

    added code to reverse a string and sentence

--- SeleniumKit/src/main/java/javaDSA/logicBuildingProblems/EasyProblems.java
+++ SeleniumKit/src/main/java/javaDSA/logicBuildingProblems/EasyProblems.java
@@ -2,15 +2,40 @@ package javaDSA.logicBuildingProblems;
 
 public class EasyProblems {
 
-//    Given a number n, find the sum of its digits.
-
-    public int sumOfDigit(int n){
-        int sizeOfDigit=Integer.toString(n).length();
-        int sum=0;
-        for(int i=0;i<sizeOfDigit;i++){
-            sum = sizeOfDigit --;
+    //    Given a number n, find the sum of its digits.
+    public int sumOfDigit(int n) {
+        int sizeOfDigit = Integer.toString(n).length();
+        int sum = 0;
+        for (int i = 0; i < sizeOfDigit; i++) {
+            sum = sizeOfDigit--;
         }
         return sum;
+    }
+
+    public String reverseAString(String string) {
+        // get the length of trhe string and store in INT variable
+        // iterate over the string and the all the characters from last to first and store in a temp variable
+        int sizeOfString = string.length();
+        String temp = "";
+        for (int i = sizeOfString - 1; i >= 0; i--) {
+            temp = temp + string.charAt(i);
+        }
+//        System.out.println("Using for loop we get:" + temp);
+//        temp = new StringBuilder(temp).reverse().toString();
+//        System.out.println("Using reverse method we get::" + temp);
+        return temp;
+    }
+
+    public void reverseSentence(String sentence) {
+        EasyProblems easyProblems = new EasyProblems();
+        String[] str = sentence.split(" ");
+        String temp = "";
+        for (int i = str.length - 1; i >= 0; i--) {
+            String reversed = easyProblems.reverseAString(str[i]);
+            temp = temp + reversed + " ";
+        }
+        System.out.println("After reversing sentence::" + temp);
+
     }
 
 
--- SeleniumKit/src/main/java/org/example/Main.java
+++ SeleniumKit/src/main/java/org/example/Main.java
@@ -1,16 +1,18 @@
 package org.example;
 
 import javaDSA.logicBuildingProblems.BasicProblems;
+import javaDSA.logicBuildingProblems.EasyProblems;
 
 public class Main {
     public static void main(String[] args) {
-
+        EasyProblems easy = new EasyProblems();
         BasicProblems BP = new BasicProblems();
 //        BP.checkEvenOrOdd(15);
 //        BP.multiplicationTable(20);
 //        System.out.println(BP.sumOfNatural2(20));
+//        easy.reverseAString("Sandeep");
 
-
+        easy.reverseSentence("sandeep is an QA");
 
 
 
