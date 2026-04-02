b5ea4bbe7c3a30b11c8fa7d2de14e9b15a098216
sandeeprepogit <san8784@gmail.com>
2026-04-01 18:43:10 -0700

    Updated the latest code

--- SeleniumKit/src/main/java/org/example/Main.java
+++ SeleniumKit/src/main/java/org/example/Main.java
@@ -7,12 +7,12 @@ public class Main {
     public static void main(String[] args) {
         EasyProblems easy = new EasyProblems();
         BasicProblems BP = new BasicProblems();
-//        BP.checkEvenOrOdd(15);
-//        BP.multiplicationTable(20);
-//        System.out.println(BP.sumOfNatural2(20));
-//        easy.reverseAString("Sandeep");
-//        easy.reverseSentence("sandeep is an QA");
-//        easy.factorialOfNumber(3);
+        BP.checkEvenOrOdd(15);
+        BP.multiplicationTable(20);
+        System.out.println(BP.sumOfNatural2(20));
+        easy.reverseAString("Sandeep");
+        easy.reverseSentence("sandeep is an QA");
+        easy.factorialOfNumber(3);
 
 
     }
